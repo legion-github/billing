0c29e392559f64cbb4e9db976e3135e1128f605f
Alexey Gladkov <gladkov.alexey@gmail.com>
2012-04-26 17:56:05 +0400

    private/tariff_rate: Optionally, class is initialized by external data and add get_* functions
    Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

--- lib/bc/private/tariff_rate.py
+++ lib/bc/private/tariff_rate.py
@@ -6,57 +6,48 @@ import uuid
 import readonly
 import bobject
 
-from rate import Rate
-
 from c2 import mongodb
 
 class TariffRateConstants(object):
 	__metaclass__ = readonly.metaClass
 	__readonly__  = {
-		CURRENCY_RUB: 'RUB',
-		CURRENCY_USD: 'USD',
-		CURRENCY_EUR: 'EUR',
+		'CURRENCY_RUB': 'RUB',
+		'CURRENCY_USD': 'USD',
+		'CURRENCY_EUR': 'EUR',
 
-		STATE_ACTIVE:  'active',
-		STATE_ARCHIVE: 'archive',
-		STATE_UPDATE:  'update',
+		'STATE_ACTIVE':  'active',
+		'STATE_ARCHIVE': 'archive',
+		'STATE_UPDATE':  'update',
 	}
 
 constants = TariffRateConstants()
 
 class TariffRate(bobject.BaseObject):
-	def __init__(self, tariff_id = None, mtype = None):
+	def __init__(self, data = None):
 
 		c = TariffRateConstants()
 
 		self.__dict__['values'] = {
-			'rid':          '',
+			'rid':          str(uuid.uuid4()),
 			'description':  '',
 			'mtype':        '',
-			'tariff_id':    '',
-			'rate':         { Rate(0), c.CURRENCY_RUB },
+			'tid':          '',
+			'rate':         {
+				'',
+				c.CURRENCY_RUB
+			},
 			'state':        c.STATE_ACTIVE,
 			'time_create':  int(time.time()),
 			'time_destroy': 0,
 			'arg':          ''
 		}
 
-		if not mtype:
-			return
-
-		o = mongodb.billing_collection('rates').find_one(
-			{
-				'mtype':     mtype,
-				'tariff_id': tariff_id,
-			}
-		)
-		if not o:
-			raise ValueError('Unknown rate')
-		self.set(o)
+		if data:
+			self.set(data)
 
 
 	def validate(self):
-		for n in ['rid', 'mtype', 'tariff_id', 'description']:
+		for n in ['rid', 'mtype', 'tid', 'description']:
 			if n not in self.__dict__['values']:
 				return False
 		return True
@@ -68,4 +59,30 @@ class TariffRate(bobject.BaseObject):
 			raise ValueError('Invalid rate')
 
 		mongodb.billing_collection('rates').insert(self.values, safe = True)
+
+
+def get_by_tariff(tid):
+	ret = []
+	for o in mongodb.billing_collection('rates').find({ 'tid': tid }):
+		ret.append(TariffRate(o))
+	return ret
+
+
+def get_by_id(rid):
+	o = mongodb.billing_collection('rates').find_one({ 'rid': rid })
+	if o:
+		return TariffRate(o)
+	return None
+
+
+def get_by_metric(tid, mtype):
+	o = mongodb.billing_collection('rates').find_one(
+		{
+			'tid':   tid,
+			'mtype': mtype
+		}
+	)
+	if o:
+		return TariffRate(o)
+	return None
 
