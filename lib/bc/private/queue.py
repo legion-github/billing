cf89b36e4131dd120423711d536473be1a3c7c46
Alexey Gladkov <gladkov.alexey@gmail.com>
2012-05-17 18:55:09 +0400

    private/queue: Rewrite to database
    Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

--- lib/bc/private/queue.py
+++ lib/bc/private/queue.py
@@ -1,21 +1,21 @@
 __version__ = '1.0'
 
 from bc.billing import utils
-from bc         import mongodb
+from bc         import database
 
 def resolve(mtype, tid, arg):
-	# Use sha1(mtype, tid, arg) to get shard id.
-	r = mongodb.collection('rates').find_one(
-		{
-			'mtype': mtype,
-			'tid':   tid,
-			'arg':   arg
-		}
-	)
-	if not r:
-		return (None, None)
-	return (r['rid'], r['rate'])
+	with database.DBCOnnect() as db:
+		r = db.query("SELECT rid,rate"+
+		             "  FROM rates"+
+		             " WHERE state=%s"+
+		             "   AND tariff_id=%s"+
+		             "   AND arg=%s",
+		             (mtype,tid,arg)).one()
+		if not r:
+			return (None, None)
+		return (r['rid'], r['rate'])
 
 
 def task_add(mtype, otask):
-	mongodb.collection(mtype, otask.uuid).insert(otask.values, safe=True)
+	with database.DBConnect(primarykey = otask.uuid) as db:
+		db.insert(mtype, otask.values)
