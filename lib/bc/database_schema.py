4a428fd9e0e97d139d09c5ca9d6948b4b089aee7
Alexey Gladkov <gladkov.alexey@gmail.com>
2012-07-05 17:28:51 +0400

    database_schema: Schema creation
    Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

--- lib/bc/database_schema.py
+++ lib/bc/database_schema.py
@@ -1,94 +1,223 @@
+from bc import database
 
-SCHEMA = {
-	'metrics': """
-			CREATE TABLE `{0}` (
-			  `id` varchar(128) NOT NULL,
-			  `type` varchar(32) NOT NULL,
-			  `formula` varchar(32) NOT NULL,
-			  `aggregate` tinyint(1) NOT NULL,
-			  PRIMARY KEY (`id`)
-			) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-		""",
+class DBTable(object):
+	def __init__(self, name, conn=None, options=[], columns=[], indexes=[]):
+		self.columns   = []
+		self.indexes   = []
+		self.tableopts = []
 
-	'queue': """
-			CREATE TABLE `{0}` (
-			  `id` varchar(36) NOT NULL,
-			  `customer` varchar(36) NOT NULL,
-			  `rid` varchar(36) NOT NULL,
-			  `rate` bigint(20) NOT NULL DEFAULT '0',
-			  `state` int NOT NULL,
-			  `value` bigint(8) NOT NULL DEFAULT '1',
-			  `time_check` int(11) NOT NULL,
-			  `time_create` int(11) NOT NULL,
-			  `time_destroy` int(11) NOT NULL DEFAULT '0',
-			  `target_user` varchar(36) DEFAULT '',
-			  `target_uuid` varchar(36) DEFAULT '',
-			  `target_descr` varchar(36) DEFAULT '',
-			  PRIMARY KEY (`id`),
-			  UNIQUE KEY `id_UNIQUE` (`id`),
-			  UNIQUE KEY `target_uuid_UNIQUE` (`target_uuid`),
-			  KEY `search_INDEX` USING BTREE (`state`,`time_check`)
-			) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-		""",
-	'rates': """
-			CREATE TABLE `{0}` (
-			  `id` varchar(36) NOT NULL,
-			  `description` varchar(1024) NOT NULL,
-			  `metric_id` varchar(128) NOT NULL,
-			  `tariff_id` varchar(36) NOT NULL,
-			  `rate` bigint(20) NOT NULL,
-			  `currency` int NOT NULL,
-			  `state` int NOT NULL,
-			  `time_create` int(11) NOT NULL,
-			  `time_destroy` int(11) NOT NULL,
-			  PRIMARY KEY (`id`),
-			  UNIQUE KEY `id_UNIQUE` (`id`),
-			  UNIQUE KEY `main_UNIQUE` USING BTREE (`state`,`metric_id`,`tariff_id`)
-			) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-		""",
-	'tariffs': """
-			CREATE TABLE `{0}` (
-			  `id` varchar(36) NOT NULL,
-			  `name` varchar(64) NOT NULL,
-			  `description` varchar(1024) NOT NULL,
-			  `time_create` int(11) NOT NULL,
-			  `time_destroy` int(11) NOT NULL,
-			  `state` int NOT NULL,
-			  PRIMARY KEY (`id`),
-			  UNIQUE KEY `id_UNIQUE` (`id`),
-			  UNIQUE KEY `main_UNIQUE` USING BTREE (`state`,`id`)
-			) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-		""",
-	'customers': """
-			CREATE TABLE `{0}` (
-			  `id` varchar(36) NOT NULL,
-			  `login` varchar(64) NOT NULL,
-			  `name_short` varchar(255)  NOT NULL,
-			  `name_full` varchar(1024) NOT NULL DEFAULT '',
-			  `comment` varchar(1024) NOT NULL DEFAULT '',
-			  `contract_client` varchar(255) NOT NULL DEFAULT '',
-			  `contract_service` varchar(255) NOT NULL DEFAULT '',
-			  `tariff_id` varchar(36) NOT NULL DEFAULT '',
-			  `contact_person` varchar(255) NOT NULL DEFAULT '',
-			  `contact_email` varchar(255) NOT NULL DEFAULT '',
-			  `contact_phone` varchar(30) NOT NULL DEFAULT '',
-			  `state` int NOT NULL DEFAULT '0',
-			  `time_create` int(11) NOT NULL,
-			  `time_destroy` int(11) NOT NULL DEFAULT '0',
-			  `wallet` bigint NOT NULL DEFAULT '0',
-			  `wallet_mode` int NOT NULL DEFAULT '0',
-			  PRIMARY KEY (`id`),
-			  UNIQUE KEY `id_UNIQUE` (`id`),
-			  UNIQUE KEY `main_UNIQUE` (`login`, `state`)
-			) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-	""",
-	'auth': """
-			CREATE TABLE `{0}` (
-			  `role` varchar(64) NOT NULL,
-			  `method` varchar(64) NOT NULL,
-			  `secret` varchar(1024) NOT NULL,
-			  `host` varchar(255) NOT NULL DEFAULT '',
-			  UNIQUE KEY `main_UNIQUE` USING BTREE (`role`, `method`)
-			) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-	""",
-}
+		self.conn = conn
+		self.name = name
+		self.set_table_options(options)
+
+		for o in columns:
+			# ( name, type [, constraint ] )
+			if len(o) < 2:
+				continue
+			self.columns.append(o)
+
+		for o in indexes:
+			self.add_index(o['cols'], **o.get('opts', dict()))
+
+
+	def set_table_options(self, arr):
+		if len(arr) > 0:
+			self.tableopts.extend(arr)
+
+
+	def sql_create_table(self):
+		res = [ "CREATE TABLE IF NOT EXISTS" ]
+		res.append(self.name)
+
+		cols = []
+		for c in self.columns:
+			cols.append(' '.join(c))
+		res.append('(' + ', '.join(cols) + ')')
+		res.extend(self.tableopts)
+
+		yield ' '.join(res) + ';'
+
+
+	def sql_create_indexes(self):
+		for idx in self.indexes:
+			res = [ "CREATE" ]
+			if idx['unique'] == True:
+				res.append("UNIQUE")
+			res.extend([ "INDEX", idx['name'] ])
+			res.extend([ "ON", self.name ])
+			res.extend([ "USING", idx['method'] ])
+			res.append( "(" + ", ".join(idx['cols']) + ")")
+			yield " ".join(res) + ";"
+
+
+	def sql_drop_table(self):
+		yield 'DROP TABLE IF EXISTS ' + self.name
+
+
+	def sql_drop_indexes(self):
+		for idx in self.indexes:
+			yield 'DROP INDEX IF EXISTS ' + idx['name']
+
+
+	def add_index(self, cols, name=None, method=None, unique=False):
+		iner_name = []
+		arr = []
+		for c in cols: # ( name, sort_order )
+			n = c[0]
+			s = len(c) > 1 and c[1] or 'ASC'
+
+			if s not in [ 'ASC', 'DESC' ]:
+				continue
+
+			iner_name.append(n)
+			arr.append( n + ' ' + s)
+
+		iner_name.sort()
+		iner_name.insert(0, self.name)
+		if unique:
+			iner_name.append('unique')
+		iner_name.append('index')
+
+		self.indexes.append(
+			{
+				'name':   name or '_'.join(iner_name),
+				'cols':   arr,
+				'method': 'BTREE',
+				'unique': bool(unique)
+			}
+		)
+
+
+	def sql_create(self):
+		for s in self.sql_create_table():
+			yield s
+		for s in self.sql_create_indexes():
+			yield s
+
+
+	def sql_drop(self):
+		for s in self.sql_drop_indexes():
+			yield s
+		for s in self.sql_drop_table():
+			yield s
+
+
+	def create(self, conn):
+		connect = conn or self.conn
+		for s in self.sql_create():
+			connect.execute(s)
+
+
+	def drop(self, conn):
+		connect = conn or self.conn
+		for s in self.sql_drop():
+			connect.execute(s)
+
+
+SCHEMA = [
+	DBTable("metrics",
+			columns = [
+				("id",        "varchar(128)", "NOT NULL PRIMARY KEY"),
+				("type",      "varchar(32)",  "NOT NULL"),
+				("formula",   "varchar(32)",  "NOT NULL"),
+				("aggregate", "int",          "NOT NULL"),
+			]
+		),
+	DBTable("queue",
+			columns = [
+				("id",          "varchar(36)",  "NOT NULL PRIMARY KEY"),
+				("customer",    "varchar(36)",  "NOT NULL"),
+				("rid",         "varchar(36)",  "NOT NULL"),
+				("rate",        "bigint",       "NOT NULL DEFAULT '0'"),
+				("state",       "int",          "NOT NULL"),
+				("value",       "bigint",       "NOT NULL"),
+				("time_check",  "int",          "NOT NULL"),
+				("time_create", "int",          "NOT NULL"),
+				("time_destroy","int",          "NOT NULL DEFAULT '0'"),
+				("target_user", "varchar(36)",  "DEFAULT ''"),
+				("target_uuid", "varchar(36)",  "DEFAULT ''"),
+				("target_descr","varchar(36)",  "DEFAULT ''"),
+			],
+			indexes = [
+				{ "cols": [ ("state", "ASC"), ("time_check", "DESC") ] }
+			]
+		),
+	DBTable("rates",
+			columns = [
+				("id",           "varchar(36)",   "NOT NULL PRIMARY KEY"),
+				("description",  "varchar(1024)", "NOT NULL"),
+				("metric_id",    "varchar(128)",  "NOT NULL"),
+				("tariff_id",    "varchar(36)",   "NOT NULL"),
+				("rate",         "bigint",        "NOT NULL"),
+				("currency",     "int",           "NOT NULL"),
+				("state",        "int",           "NOT NULL"),
+				("time_create",  "int",           "NOT NULL"),
+				("time_destroy", "int",           "NOT NULL"),
+			],
+			indexes = [
+				{ "cols": [ ("state", "ASC"), ("metric_id", "ASC"), ("tariff_id","ASC") ] }
+			]
+		),
+	DBTable("tariffs",
+			columns = [
+				("id",          "varchar(36)",   "NOT NULL PRIMARY KEY"),
+				("name",        "varchar(64)",   "NOT NULL"),
+				("description", "varchar(1024)", "NOT NULL"),
+				("time_create", "int",           "NOT NULL"),
+				("time_destroy","int",           "NOT NULL"),
+				("state",       "int",           "NOT NULL"),
+			],
+			indexes = [
+				{ "cols": [ ("state", "ASC") ] }
+			]
+		),
+	DBTable("customers",
+			columns = [
+				("id",              "varchar(36)",   "NOT NULL PRIMARY KEY"),
+				("login",           "varchar(64)",   "NOT NULL"),
+				("name_short",      "varchar(255)",  "NOT NULL"),
+				("name_full",       "varchar(1024)", "NOT NULL DEFAULT ''"),
+				("comment",         "varchar(1024)", "NOT NULL DEFAULT ''"),
+				("contract_client", "varchar(255)",  "NOT NULL DEFAULT ''"),
+				("contract_service","varchar(255)",  "NOT NULL DEFAULT ''"),
+				("tariff_id",       "varchar(36)",   "NOT NULL DEFAULT ''"),
+				("contact_person",  "varchar(255)",  "NOT NULL DEFAULT ''"),
+				("contact_email",   "varchar(255)",  "NOT NULL DEFAULT ''"),
+				("contact_phone",   "varchar(30)",   "NOT NULL DEFAULT ''"),
+				("state",           "int",           "NOT NULL DEFAULT '0'"),
+				("time_create",     "int",           "NOT NULL"),
+				("time_destroy",    "int",           "NOT NULL DEFAULT '0'"),
+				("wallet_mode",     "int",           "NOT NULL DEFAULT '0'"),
+				("wallet",          "bigint",        "NOT NULL DEFAULT '0'"),
+			],
+			indexes = [
+				{ "cols": [ ("state", "ASC") ] }
+			]
+		),
+	DBTable("auth",
+			columns = [
+				("id",    "varchar(36)",   "NOT NULL PRIMARY KEY"),
+				("role",  "varchar(64)",   "NOT NULL"),
+				("method","varchar(64)",   "NOT NULL"),
+				("secret","varchar(1024)", "NOT NULL"),
+				("host",  "varchar(255)",  "NOT NULL DEFAULT ''"),
+			],
+			indexes = [
+				{ "cols": [ ("role", "ASC"), ("method", "ASC") ] }
+			]
+		),
+]
+
+def create_schema(dbname=None, dbuser=None, dbpass=None):
+	with database.DBConnect(dbname=dbname, dbuser=dbuser, dbpass=dbpass, commit=False) as db:
+		for table in SCHEMA:
+			table.create(db)
+		db.commit()
+
+
+def destroy_schema(dbname=None, dbuser=None, dbpass=None):
+	with database.DBConnect(dbname=dbname, dbuser=dbuser, dbpass=dbpass, commit=False) as db:
+		for table in SCHEMA:
+			table.drop(db)
+		db.commit()
+
