082efebe22c0109f82a90a3c71f5a7b58d9f286a
nekolyanich <gmail@nekolyanich.com>
2012-10-04 13:52:36 +0400

    added multihost support in client library

--- lib/bc_client/__init__.py
+++ lib/bc_client/__init__.py
@@ -1,55 +1,102 @@
 import client
 
+def configcreator(hosts_auth, local, method_returning):
+	"""For lazy administrators with same auth on diffrent wapis"""
+	ans = {}
+	for key,value in method_returning.iteritems():
+		ans[key]={
+			'local': local,
+			'hosts': hosts_auth,
+			'returning': value
+			}
+	return ans
 
-def Customers(host, auth, timeout):
-	return client.BCClient(host, auth, timeout, {
-		'customerList':    'customers',
-		'customerGet':     'customer',
-		'customerAdd':     'id',
-		'customerModify':  'status',
-		'customerRemove':  'status',
-		'customerDeposit': 'status',
-		})
+
+def Client(config):
+	"""
+	config sample:
+	{
+	...
+	'wapiMethodName': {
+		'returning': 'returningData',
+		'hosts': {
+			'HOST1:PORT':{
+				"role": "admin",
+				"secret": "qwerty"
+			},
+			'HOST2:PORT':{
+				"role": "user",
+				"secret": "123456"
+			},
+		},
+		'local': 'HOST1:PORT'
+		},
+	...
+	}
+	"""
+	return client.BCClient(config)
 
 
-def Metrics(host, auth, timeout):
-	return client.BCClient(host, auth, timeout, {
+def Customers(hosts_auth, local):
+	return client.BCClient(configcreator(
+		hosts_auth, local,
+		{
+			'customerList':    'customers',
+			'customerGet':     'customer',
+			'customerAdd':     'id',
+			'customerModify':  'status',
+			'customerRemove':  'status',
+			'customerDeposit': 'status',
+		}))
+
+
+def Metrics(hosts_auth, local):
+	return client.BCClient(configcreator(
+		hosts_auth, local,
+		{
 		'metricList':'metrics',
 		'metricAdd': 'id',
 		'metricGet': 'metric',
-		})
+		}))
 
 
-def Rates(host, auth, timeout):
-	return client.BCClient(host, auth, timeout, {
+def Rates(hosts_auth, local):
+	return client.BCClient(configcreator(
+		hosts_auth, local,
+		{
 		'rateList':  'rates',
 		'rateGet':   'rate',
 		'rateAdd':   'id',
 		'rateModify':'status',
 		'rateRemove':'status',
-		})
+		}))
 
 
-def Tariffs(host, auth, timeout):
-	return client.BCClient(host, auth, timeout, {
+def Tariffs(hosts_auth, local):
+	return client.BCClient(configcreator(
+		hosts_auth, local,
+		{
 		'tariffList':       'tariffs',
 		'tariffGet':        'tariff',
 		'tariffAdd':        'id',
 		'tariffModify':     'status',
 		'tariffRemove':     'status',
-		})
+		}))
 
 
-def Tasks(host, auth, timeout):
-	return client.BCClient(host, auth, timeout, {
+def Tasks(hosts_auth, local):
+	return client.BCClient(configcreator(
+		hosts_auth, local,
+		{
 		'taskAdd':   'id',
 		'taskModify':'status',
 		'taskRemove':'status',
-		})
+		}))
 
-def Sync(host, auth, timeout):
-	return client.BCClient(host, auth, timeout, {
+def Sync(hosts_auth, local):
+	return client.BCClient(configcreator(
+		hosts_auth, local,
+		{
 		'sync':    'status',
 		'syncList':'status',
-		})
-
+		}))
