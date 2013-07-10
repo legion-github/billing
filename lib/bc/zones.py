ddc106be163565dd7a902778d45438d9a94f4ea6
Alexey Gladkov <gladkov.alexey@gmail.com>
2013-07-10 15:14:55 +0400

    Fix typo after rename of config value
    Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

--- lib/bc/zones.py
+++ lib/bc/zones.py
@@ -3,7 +3,7 @@ from bc import hashing
 
 def write_zone(key):
 	conf = config.read()
-	data = config.subdict(conf['zone'], field='weight')
+	data = config.subdict(conf['zones'], field='weight')
 	ring = hashing.HashRing(data.keys(), data)
 	res  = ring.get_node(key)
-	return conf['zone'][res] 
+	return conf['zones'][res]
